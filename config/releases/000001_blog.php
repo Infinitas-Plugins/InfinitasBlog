2a7856dd9f130dc7fcc8685f3befb34d35e85181
dogmatic69 <dogmatic69@gmail.com>
2010-11-11 17:12:38 +0000

    creating a release for the blog plugin

---
+++ config/releases/000001_blog.php
@@ -0,0 +1,85 @@
+<?php
+class R4cdc2222e7544419a3ce247a6318cd70 extends CakeRelease {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = 'Migration for Blog version 0.1';
+
+/**
+ * Plugin name
+ *
+ * @var string
+ * @access public
+ */
+	public $plugin = 'Blog';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'posts' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+					'title' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'body' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'comment_count' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+					'active' => array('type' => 'boolean', 'null' => false, 'default' => '0', 'key' => 'index'),
+					'views' => array('type' => 'integer', 'null' => false, 'default' => '0', 'key' => 'index'),
+					'rating' => array('type' => 'float', 'null' => false, 'default' => '0'),
+					'rating_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+					'parent_id' => array('type' => 'integer', 'null' => true, 'default' => '0'),
+					'ordering' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+					'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+					'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+					'category_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'index'),
+					'tags' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+					'indexes' => array(
+						'PRIMARY' => array('column' => 'id', 'unique' => 1),
+						'category_id' => array('column' => 'category_id', 'unique' => 0),
+						'active' => array('column' => 'active', 'unique' => 0),
+						'most_views' => array('column' => array('views', 'id', 'title'), 'unique' => 0),
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'posts'
+			),
+		),
+	);
+
+	
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
+?>
---
+++ config/releases/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000001_blog' => 'R4cdc2222e7544419a3ce247a6318cd70'),
+);
+?>
